275719a3b40a98bf7c208aa9dcf61d3f317fb8d5
achintha-cccc <achinthaudayanga26@gmail.com>
2026-01-15 20:54:02 +0530

    Update fetch_file.py

--- fetch_file.py
+++ fetch_file.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://raw.githubusercontent.com/####/Hello-python/main/hello.py"
+url = "https://raw.githubusercontent.com/%%%%/Hello-python/main/hello.py"
 response = requests.get(url)
 
 print(response.text)
